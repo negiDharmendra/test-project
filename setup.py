d07e51f80533273b30014bc333e82b8ddf52f484
Dharmendra Singh <negidharmendra.negi44@gmail.com>
2023-04-05 11:59:42 +0530

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# This file is required for the tox testing framework
+# It should reside in the same directory as 'tox.ini'
+# To use tox, just run 'tox' from the same directory as this file.
+
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup()
